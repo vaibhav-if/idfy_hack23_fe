4d3a3a2bf3c5a2475e07d18d822bc13843e5d16a
charan <charan.banka@idfy.com>
2023-05-12 01:37:01 +0530

    header setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./dashboard";
+import Home from "./home";
+import Navbar from "./navbar";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <Navbar />
+      <div className="mt10">
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          {/* <Route path="/home" element={<Home />} exact /> */}
+          <Route path="/dashboard" element={<Dashboard />} exact />
+          <Route path="*" element="No page found" exact={true} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ src/navbar/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link, useHistory, NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <header className="header sticky-header">
+      <div className="top">
+        <Link to="/" className="image">
+          <img src="/images/idfy-logo.svg" alt="Idfy Logo" />
+        </Link>
+        <div className="d-flex gap-1">
+          <ul className="header-nav">
+            <li>
+              <NavLink
+                to="/dashboard"
+                exact
+                className="nav-link"
+                activeClassName="active"
+              >
+                Dashboard <div></div>{" "}
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
